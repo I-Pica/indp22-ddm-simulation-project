683e0a2470ce75e0623fd4ed240452621c581b19
MarColnaghi <marc.colnaghi@gmail.com>
2023-02-23 09:45:17 +0000

    Revert "Update ddm.py"
    This reverts commit 435cfb35479d8bcdf1dda094ad8f64fef27385ec.

--- code/ddm.py
+++ code/ddm.py
@@ -187,4 +187,3 @@ def plot_trajectories_and_RT(S, hits, errs, traj, mu, t, bins, colors):
    plt.subplots_adjust(hspace=0)
    plt.xlabel('Time from Onset (s)');
    plt.ylabel('Error Count');
-   
